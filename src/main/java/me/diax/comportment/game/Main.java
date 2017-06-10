478d4d3c49cc45e4fb94b379acf2702db09fe647
Comportment <Comportment@protonmail.com>
2017-06-10 12:09:49 +0100

    here is a window

--- src/main/java/me/diax/comportment/game/Main.java
+++ src/main/java/me/diax/comportment/game/Main.java
@@ -1,6 +1,9 @@
 package me.diax.comportment.game;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
 /**
  * Created by Comportment on 10/06/2017 at 11:17
@@ -14,9 +17,23 @@ public class Main {
 
     public void main() {
         JFrame frame = new JFrame("GamE");
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(200, 200));
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setIconImage(getIcon());
         frame.setResizable(true);
+        frame.setContentPane(panel);
         frame.pack();
         frame.setVisible(true);
+    }
+
+
+    public Image getIcon() {
+        try {
+            return ImageIO.read(getClass().getResourceAsStream("/icon.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
